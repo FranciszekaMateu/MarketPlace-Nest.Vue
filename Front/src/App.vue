4683f29ff5de4834a729e648e997ed8dfad6a6c8
FranciszekaMateu <franciszekamataio@gmail.com>
2023-09-27 20:15:22 -0300

    Agregando Navbar

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,13 +1,18 @@
 <template>
     <div id="app">
-      
-      <router-view/>
+      <Navbar/>
+        <router-view/>
     </div>
   </template>
   
   <script>
+  import Navbar from './components/Navbar.vue'; 
+  
   export default {
     name: 'App',
+    components: {
+      Navbar
+    }
   }
   </script>
   
---
+++ Front/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar app color="indigo" dark>
+      <v-toolbar-title>Mi Ecommerce</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text to="/">Inicio</v-btn>
+      <v-btn text to="/productos">Productos</v-btn>
+      <v-btn text to="/contacto">Contacto</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  }
+  </script>
+  
