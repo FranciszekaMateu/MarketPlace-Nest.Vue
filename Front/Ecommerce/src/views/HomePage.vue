545c7d07672a3de1d21635a5e8b8c8f01f2dc2e2
FranciszekaMateu <franciszekamataio@gmail.com>
2023-09-27 18:39:41 -0300

    First commit

---
+++ Front/Ecommerce/src/views/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Bienvenido a Mi eCommerce</h1>
+    <p>Encuentra los mejores productos a los mejores precios.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+  data() {
+    return {
+      products: [] 
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+    p {
+      font-size: 2em;
+      text-align: center;
+      color: black;
+    }
+</style>
